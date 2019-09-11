3fcb001d119bc3f03452624c3b25502793c5b3db
Matt West <git@matthewwest.co.uk>
2019-09-11 08:24:42 +0100

    Add clear() method to QTranslator.
    [ChangeLog][QtCore][Translator] Expose the previously private QTranslator
    clear() method.  This allows QTranslator objects to be explicitly cleared.

--- src/corelib/kernel/qtranslator.h
+++ src/corelib/kernel/qtranslator.h
@@ -16,6 +16,8 @@ QT_BEGIN_NAMESPACE
 class QLocale;
 class QTranslatorPrivate;
 
+#define HAS_QTRANSLATOR_CLEAR_METHOD
+
 class Q_CORE_EXPORT QTranslator : public QObject
 {
     Q_OBJECT
@@ -27,6 +29,7 @@ public:
                               const char *disambiguation = nullptr, int n = -1) const;
 
     virtual bool isEmpty() const;
+    virtual void clear();
 
     QString language() const;
     QString filePath() const;
